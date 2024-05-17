bece6aae2b55cec9e38a86cfb74dd138740fbfeb
Jashor <jasherabraham@outlook.es>
2024-05-17 16:29:57 -0600

    Practice 10

--- Practice10/src/ejemploexepciones/PruebaExcepcion.java
+++ Practice10/src/ejemploexepciones/PruebaExcepcion.java
@@ -1,26 +1,27 @@
 package ejemploexepciones;
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-class PruebaExcepcion {
-public static void main( String[] args ) {
-// Para leer un archivo
-FileInputStream entrada = null;
-//creamos un objeto de la clase Lanzadora
-Lanzadora lanza = new Lanzadora();
-int leo;
-try {
-entrada = new FileInputStream("archivo.txt");
-while ( ( leo = entrada.read() ) != -1 )
-//utilizamos el métodos de la clase Lanzadora
-lanza.lanzaSiNegativo( leo );
-System.out.println("Todo fue bien");
-} catch ( Miexcepcion e ){ // Personalizada
-System.out.println("Excepcion presentada: " + e.getMessage() );
-} catch ( IOException e ){ // Estándar
-System.out.println("Excepcion presentada: " + e.getMessage() );
-}
-}
+
+class PruebaException{
+    public static void main(String[] args) {
+        //Para leer el archivo
+        FileInputStream entrada= null;
+        //Creamos un objeto de la clase Lanzadora
+        Lanzadora lanza = new Lanzadora();
+        int leo;
+        try{
+            entrada = new FileInputStream("archivo.txt");
+            while((leo = entrada.read()) != -1){
+                //Utilizamos el métodos de la clase Lanzadora
+                lanza.lanzaSiNegativo(leo);
+               }   System.out.println("Todo fue bien");
+            } catch (Miexcepcion e){
+                //Personalizada
+                System.out.println("Excepción presentada: " + e.getMessage());
+
+            }catch (Exception e){
+                //General
+                System.out.println("Excepción presentada: " + e.getMessage());
+        }
+    }
 }
