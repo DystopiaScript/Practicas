be402b944021d7d62f63890a8acb3852d9456c8d
Jashor <jasherabraham@outlook.es>
2024-05-17 21:12:37 -0600

    Restructuration

--- Practice7/src/musica/InstrumentosMusicales.java
+++
@@ -1,7 +0,0 @@
-package musica;
-abstract class InstrumentosMusicales {
-    //Atributo de tipo String
-    public String tipodeinstrumento;
-    //Método abstracto
-    public abstract void tocarInstrumento();
-}
