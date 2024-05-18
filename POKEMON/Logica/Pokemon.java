44e0cac736dcbe83e9294f358e0b980033770b40
Jashor <jasherabraham@outlook.es>
2024-05-18 12:43:12 -0600

    I am boring, okay?

---
+++ POKEMON/Logica/Pokemon.java
@@ -0,0 +1,12 @@
+package POKEMON.Logica;    
+public abstract class Pokemon {
+    protected int numPokedex;
+    protected String nombre;
+    protected double sexo;
+    protected int temporada;
+
+    protected abstract void ataquePlacaje();
+    protected abstract void ataqueAranizado();
+    protected abstract void ataqueMoridisco();
+ 
+}
