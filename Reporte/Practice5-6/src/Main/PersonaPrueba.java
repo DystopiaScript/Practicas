dcc7f40486a4f8bba5a07623317f61b652c8d82b
Jashor <jasherabraham@outlook.es>
2024-05-31 08:46:47 -0600

    I am so boring, okay?

--- Reporte/Practice5-6/src/Main/PersonaPrueba.java
+++ Reporte/Practice5-6/src/Main/PersonaPrueba.java
@@ -5,19 +5,23 @@ import persona.Claseprincipal.Herencia.EmpleadoOperativo;
 
 public class PersonaPrueba {
     public static void main(String[] args) {
+        //Declaración de variables
         int opcion;
         String nombre, domicilio, telefono, curp, rfc;
         float sueldobase, sueldoneto, precioporhora, imp;
         int edad, horastrabajadas;
+        //Creación de objetos
         EmpleadoAdministrativo EA1 = new EmpleadoAdministrativo();
         EmpleadoOperativo EO1 = new EmpleadoOperativo();
+        //Ciclo while para mostrar el menú de opciones
         while (true) {
             opcion=Integer.parseInt(JOptionPane.showInputDialog("Menú de Opciones \n"
-           + "1.- Alta Empleado Operativo\n"
+           + "1.- Alta Empleado Adminstrativo\n"
            +"2.- Cálculo de Sueldo Empleado Adminstrativo\n"
            +"3.- Listar datos Empleados Operativo\n"
            +"4.- Salir" ));
            switch (opcion) {
+            //Caso 1, pide los datos del empleado administrativo
             case 1:
                 nombre=JOptionPane.showInputDialog("Nombre del Empleado: ");
                 edad=Integer.parseInt(JOptionPane.showInputDialog("Edad: "));
@@ -25,21 +29,30 @@ public class PersonaPrueba {
                 telefono=JOptionPane.showInputDialog("Teléfono: ");
                 horastrabajadas=Integer.parseInt(JOptionPane.showInputDialog("Horas Trabajadas: "));
                 precioporhora=Float.parseFloat(JOptionPane.showInputDialog("Precio por Hora:"));
+                sueldobase=Float.parseFloat(JOptionPane.showInputDialog("Sueldo Base: "));
                 curp=JOptionPane.showInputDialog("CURP: ");
                 rfc=JOptionPane.showInputDialog("RFC: ");
+                //Calculo del sueldo neto
                 sueldoneto=horastrabajadas*precioporhora;
-                EO1.altaTrabajador(nombre, domicilio, telefono, curp, rfc, edad, sueldoneto, precioporhora, horastrabajadas);
+                //Metodo para dar de alta al empleado
+                EO1.altaTrabajador(nombre, domicilio, telefono, curp, rfc, edad, sueldobase, sueldoneto, precioporhora, horastrabajadas);
+                break;
+                //Caso 2, pide el sueldo base y calcula el sueldo neto del empleado administrativo
                 case 2:
                 sueldobase=Float.parseFloat(JOptionPane.showInputDialog("Sueldo base: "));
                 imp=sueldobase*.32f;
                 JOptionPane.showMessageDialog(null, "El sueldo Neto del Empleado es : "
+                //Metodo para calcular el sueldo neto
                 +EA1.CalculodeSueldoNeto(sueldobase, imp));
                 break;
+                //Lista los datos del empleado operativo
                 case 3:
                 EO1.listarDatos();
                 break;
+                //Cierra el programa
                 case 4:
                 System.exit(0);
+                //Mensaje de error
             default:
             System.out.println("Selecciona una opcion correcta (1-4)");
                 break;
